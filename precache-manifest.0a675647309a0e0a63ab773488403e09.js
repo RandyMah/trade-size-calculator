4bb54a69bff115e4e29ad4e7a05b5bf7a02b8adf
Mah Zheng Ran <frspinner97@gmail.com>
2021-02-25 18:12:15 +0800

    Updates

---
+++ precache-manifest.0a675647309a0e0a63ab773488403e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9903462148ce40d78c8386d97b42ef8c",
+    "url": "/trade-size-calculator/index.html"
+  },
+  {
+    "revision": "4b5ad3fabe12a3ba125b",
+    "url": "/trade-size-calculator/static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "bcb706849c956605e56a",
+    "url": "/trade-size-calculator/static/css/main.564779a3.chunk.css"
+  },
+  {
+    "revision": "4b5ad3fabe12a3ba125b",
+    "url": "/trade-size-calculator/static/js/2.b837dbf5.chunk.js"
+  },
+  {
+    "revision": "bcb706849c956605e56a",
+    "url": "/trade-size-calculator/static/js/main.88469422.chunk.js"
+  },
+  {
+    "revision": "23629cea6d710418fef0",
+    "url": "/trade-size-calculator/static/js/runtime-main.799cf029.js"
+  }
+]);
